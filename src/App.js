4e76d336ebb6565b6886d471d8852de91e93bd87
rPhilip <rphilip21@gmail.com>
2017-07-06 10:36:28 -0700

    updated app.js message

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <Header/>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          ecoUp PDX is a resource to help Portlanders learn how to reduce our environmental footprint.
         </p>
       </div>
     );
