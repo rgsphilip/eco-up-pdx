bb2c485b0df2813b2c4a5cc367a9fcb4e32a462f
rPhilip <rphilip21@gmail.com>
2017-07-11 11:43:12 -0700

    Added additional explanatory line

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
           <Header/>
         </div>
         <p className="App-intro">
-          ecoUp PDX is a resource to help Portlanders learn how to reduce our environmental footprint.
+          EcoUp PDX is a resource to help Portlanders learn how to reduce our environmental footprint.
+          <br/>
+          Click on the segments below to learn more about where the average American's carbon emissions come from and how to reduce them. 
         </p>
         <div className="Bar-graph">
           <BarGraph/>
