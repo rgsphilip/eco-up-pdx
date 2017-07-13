9ba6931244a2c56ea0896a2e481cdf2b94d9d24a
rPhilip <rphilip21@gmail.com>
2017-07-13 12:26:15 -0700

    update to header to accomplish successful linking to page sections

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
 class Header extends Component {
   render() {
     return (
       <div>
          <Menu>
-          <Menu.Item name='ecoUp PDX'/>
-          <Menu.Item name='About'/>
+          <Menu.Item as={Link} to="/" name='ecoUp PDX'/>
+          <Menu.Item as={Link} to="/about" name='About'/>
           <Menu.Item name='Putting it all together'/>
         </Menu> 
       </div>
