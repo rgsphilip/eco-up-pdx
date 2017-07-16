644392a000f0214b812cc4681c1e2acdc4b55cb9
rPhilip <rphilip21@gmail.com>
2017-07-16 11:56:01 -0700

    Figured out how to link svg items, to link the rest

--- src/components/bargraph/BarGraph.js
+++ src/components/bargraph/BarGraph.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 class BarGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.props.history.push('/about');
+  }
+
   render() {
     return(
-      <div className="rectangle">
-        <svg width="40%" height="100%" className="rectangle">
+      <div>
+        <svg width="40%" height="60vh" className="rectangle" onClick={this.onClick} style={{cursor: 'pointer'}}>
           <rect width="100%" height="100%" fill="red"/>
         </svg>
-        <svg width="20%" height="100%">
+        <svg width="20%" height="60vh">
           <rect width="100%" height="100%" fill="blue"/>
         </svg>
-        <svg width="20%" height="100%">
+        <svg width="20%" height="60vh">
           <rect width="100%" height="100%" fill="green"/>
         </svg>
-        <svg width="20%" height="100%">
+        <svg width="20%" height="60vh">
           <rect width="100%" height="100%" fill="orange"/>
         </svg>
-        {/*<div className="rectangle"></div>*/}
          
       </div>
       
@@ -24,4 +33,4 @@ class BarGraph extends Component {
   }
 
 }
-export default BarGraph;
+export default withRouter(BarGraph);
